b07e14332a5bdbd6f34253d4c702e11513d2f199
Franco Ortega <francopdx@gmail.com>
2021-09-29 17:12:06 -0700

    add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/header/Header';
 import './scss/App.scss';
 
 const App = () => {
   return (
     <div>
-
+      <Header />
     </div>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <div className={styles.Header}>
+      <h1>Hello Sass and React!</h1>
+
+    </div>
+  )
+}
+
+export default Header;
