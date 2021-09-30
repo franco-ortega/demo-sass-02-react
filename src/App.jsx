c5e68fcb6345a8562ff4ae25076c61fcf6cb6ea9
Franco Ortega <francopdx@gmail.com>
2021-09-30 15:34:34 -0700

    add Image

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Image from './components/image/Image';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Home />
+      <Image />
+      <Image />
+      <Image />
     </div>
   )
 }
