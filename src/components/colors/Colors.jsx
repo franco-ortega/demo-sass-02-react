7bba1045d1501b2be4bc9acfec0653931d421ed8
Franco Ortega <francopdx@gmail.com>
2021-10-01 17:28:38 -0700

    add Colors component

---
+++ src/components/colors/Colors.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Colors.module.scss';
+
+const Colors = () => {
+  return (
+    <footer className={styles.Colors}>
+      <h2>Colors!!!!</h2>
+      <div>
+        <p>Box</p>
+        <p>Box</p>
+        <p>Box</p>
+        <p>Box</p>
+        <p>Box</p>
+      </div>
+      <div>
+        <p>Box</p>
+        <p>Box</p>
+        <p>Box</p>
+        <p>Box</p>
+        <p>Box</p>
+      </div>
+      
+    </footer>
+  )
+}
+
+export default Colors;
