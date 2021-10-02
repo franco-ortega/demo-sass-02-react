025d2691230066f9f476dc74acbe40530214e310
Franco Ortega <francopdx@gmail.com>
2021-10-01 17:46:53 -0700

    add blue and purple boxes; add SaturationBox component

--- src/components/colors/Colors.jsx
+++ src/components/colors/Colors.jsx
@@ -1,41 +1,20 @@
 import React from 'react';
 import styles from './Colors.module.scss';
+import SaturationBox from './SaturationBox';
 
 const Colors = () => {
   return (
     <footer className={styles.Colors}>
       <h2>Colors!!!!</h2>
-      <div>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-      </div>
-      <div>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-      </div>
-      <div>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-      </div>
-      <div>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-        <p>Box</p>
-      </div>
-      
+      <SaturationBox color={'Red'} />
+      <SaturationBox color={'Yellow'} />
+      <SaturationBox color={'Orange'} />
+      <SaturationBox color={'Green'} />
+      <SaturationBox color={'Blue'} />
+      <SaturationBox color={'Purple'} />
+
     </footer>
   )
-}
+};
 
 export default Colors;
