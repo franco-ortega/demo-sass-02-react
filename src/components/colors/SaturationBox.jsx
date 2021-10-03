847eac8e4a95d2e7ad2a5ba4dc37dead9d76ea7e
Franco Ortega <francopdx@gmail.com>
2021-10-02 21:22:58 -0700

    refactor boxColors mixin to SaturationBox component

--- src/components/colors/SaturationBox.jsx
+++ src/components/colors/SaturationBox.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import styles from './SaturationBox.module.scss';
 
 const SaturationBox = ({ color }) => {
+  let hsl;
+  
+  if(color === 'Red') hsl = styles.Red;
+  else if(color === 'Yellow') hsl = styles.Yellow;
+  else if(color === 'Orange') hsl = styles.Orange;
+  else if(color === 'Green') hsl = styles.Green;
+  else if(color === 'Blue') hsl = styles.Blue;
+  else if(color === 'Purple') hsl = styles.Purple;
+
   return (
-    <div>
+    <div className={hsl}>
       <p>{color} <br /> Full Sat </p>
       <p>{color} <br /> High Sat </p>
       <p>{color} <br /> Mid Sat </p>
